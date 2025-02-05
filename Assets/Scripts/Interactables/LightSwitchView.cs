e7bb552f3ac5baf2c4a3d26ec36a2ad9ed756a35
Parth Vaidya <zeusparth99@gmail.com>
2025-02-05 11:49:50 +0530

    Solid Event Architecture added

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -7,15 +7,15 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
-    public static event Action OnLightSwitchToggled;
+    // public static event Action OnLightSwitchToggled;
 
-    private void OnEnable() => OnLightSwitchToggled += onLightSwitch;
+    private void OnEnable() => EventService.Instance.onLightSwitchToggled.AddListener(onLightSwitch);
 
-    private void OnDisable() => OnLightSwitchToggled -= onLightSwitch;
+    private void OnDisable() => EventService.Instance.onLightSwitchToggled.RemoveListener(onLightSwitch);
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => OnLightSwitchToggled?.Invoke();
+    public void Interact() => EventService.Instance.onLightSwitchToggled.InvokeEvent();
 
     private void toggleLights()
     {
